b289cd7779b516ea4e0310b653dbb0f93d058dab
Mostafa Mohamed <mostafamohamedcx8@gmail.com>
2024-08-11 18:28:12 +0300

    Last webhook

--- utils/createtoken.js
+++ utils/createtoken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const createToken = (payload) =>
-  jwt.sign({ userId: payload }, process.env.JWT_SECRET, {
+  jwt.sign({ userId: payload }, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRE_TIME,
   });
 
